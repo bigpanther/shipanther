80a50ae69878aff8e4dcf2dfccd0db3b4ff9ed82
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-08 21:46:10 -0800

    Merge login and registration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/button_builder.dart';
-import 'package:shipanther/screens/register_page.dart';
-import 'package:shipanther/screens/signin_page.dart';
+import 'package:shipanther/screens/signin_or_register_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,55 +15,10 @@ class ShipantherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Shipanther',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark(),
-        home: Scaffold(
-          body: AuthTypeSelector(),
-        ));
-  }
-}
-
-/// Provides a UI to select a authentication type page
-class AuthTypeSelector extends StatelessWidget {
-  // Navigates to a new page
-  void _pushPage(BuildContext context, Widget page) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(builder: (_) => page),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Shipanther"),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: SignInButtonBuilder(
-              icon: Icons.person_add,
-              backgroundColor: Colors.indigo,
-              text: 'Registration',
-              onPressed: () => _pushPage(context, RegisterPage()),
-            ),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-          Container(
-            child: SignInButtonBuilder(
-              icon: Icons.verified_user,
-              backgroundColor: Colors.orange,
-              text: 'Sign In',
-              onPressed: () => _pushPage(context, SignInPage()),
-            ),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-        ],
-      ),
+      title: 'Shipanther',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: SignInOrRegistrationPage(AuthTypeSelector.signIn),
     );
   }
 }
