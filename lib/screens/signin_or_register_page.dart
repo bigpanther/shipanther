61d233cfc35b4c38cf523d62638f9a0d41c2dabf
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-09 17:03:33 -0800

    Fix scaffold not found error

--- lib/screens/signin_or_register_page.dart
+++ lib/screens/signin_or_register_page.dart
@@ -21,19 +21,33 @@ enum AuthTypeSelector {
   register,
 }
 
-class SignInOrRegistrationPage extends StatefulWidget {
+class SignInOrRegistrationPage extends StatelessWidget {
   final AuthTypeSelector authTypeSelector;
+  final String title;
   SignInOrRegistrationPage(this.authTypeSelector)
       : this.title = (authTypeSelector == AuthTypeSelector.register)
             ? 'Register'
             : 'Sign In';
-  final String title;
-
   @override
-  State<StatefulWidget> createState() => _SignInOrRegistrationPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: SignInOrRegistrationForm(authTypeSelector),
+    );
+  }
 }
 
-class _SignInOrRegistrationPageState extends State<SignInOrRegistrationPage> {
+class SignInOrRegistrationForm extends StatefulWidget {
+  final AuthTypeSelector authTypeSelector;
+  SignInOrRegistrationForm(this.authTypeSelector);
+
+  @override
+  State<StatefulWidget> createState() => _SignInOrRegistrationFormState();
+}
+
+class _SignInOrRegistrationFormState extends State<SignInOrRegistrationForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   String _userEmail;
@@ -41,171 +55,160 @@ class _SignInOrRegistrationPageState extends State<SignInOrRegistrationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Form(
-          key: _formKey,
-          child: Card(
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Email'),
-                    autocorrect: false,
-                    enableSuggestions: false,
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (String value) {
-                      if (value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      return null;
-                    },
-                    onSaved: (val) => setState(() => _userEmail = val),
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Password'),
-                    validator: (String value) {
-                      if (value.isEmpty) {
-                        return 'Please enter a password';
-                      }
-                      return null;
-                    },
-                    obscureText: true,
-                    onSaved: (val) => setState(() => _password = val),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    alignment: Alignment.center,
-                    child: SignInButtonBuilder(
-                      icon:
-                          (widget.authTypeSelector == AuthTypeSelector.register)
-                              ? Icons.person_add
-                              : Icons.verified_user,
-                      backgroundColor:
-                          (widget.authTypeSelector == AuthTypeSelector.register)
-                              ? Colors.blueGrey
-                              : Colors.orange,
-                      onPressed: () async {
-                        if (_formKey.currentState.validate()) {
-                          _formKey.currentState.save();
-                          if (widget.authTypeSelector ==
-                              AuthTypeSelector.register) {
-                            var success =
-                                await _register(context, _userEmail, _password);
-                            if (success) {
-                              _signInWithEmailAndPassword(
-                                  context, _userEmail, _password);
-                            }
-                          } else {
-                            _signInWithEmailAndPassword(
-                                context, _userEmail, _password);
+    return Form(
+        key: _formKey,
+        child: Card(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  autocorrect: false,
+                  enableSuggestions: false,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                  onSaved: (val) => setState(() => _userEmail = val),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  onSaved: (val) => setState(() => _password = val),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  alignment: Alignment.center,
+                  child: SignInButtonBuilder(
+                    icon: (widget.authTypeSelector == AuthTypeSelector.register)
+                        ? Icons.person_add
+                        : Icons.verified_user,
+                    backgroundColor:
+                        (widget.authTypeSelector == AuthTypeSelector.register)
+                            ? Colors.blueGrey
+                            : Colors.orange,
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+                        if (widget.authTypeSelector ==
+                            AuthTypeSelector.register) {
+                          var success = await _register(_userEmail, _password);
+                          if (success) {
+                            _signInWithEmailAndPassword(_userEmail, _password);
                           }
+                        } else {
+                          _signInWithEmailAndPassword(_userEmail, _password);
                         }
-                      },
-                      text:
-                          (widget.authTypeSelector == AuthTypeSelector.register)
-                              ? 'Register'
-                              : "Sign In",
-                    ),
+                      }
+                    },
+                    text: (widget.authTypeSelector == AuthTypeSelector.register)
+                        ? 'Register'
+                        : "Sign In",
                   ),
-                  Container(
-                    alignment: Alignment.center,
-                    child: RichText(
-                      text: TextSpan(
-                          text: (widget.authTypeSelector ==
-                                  AuthTypeSelector.register)
-                              ? "Sign In"
-                              : 'Register',
-                          style: TextStyle(color: Colors.blue),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              Navigator.of(context).pushReplacement(
-                                MaterialPageRoute<void>(
-                                    builder: (_) => SignInOrRegistrationPage(
-                                        widget.authTypeSelector ==
-                                                AuthTypeSelector.register
-                                            ? AuthTypeSelector.signIn
-                                            : AuthTypeSelector.register)),
-                              );
-                            }),
-                    ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: RichText(
+                    text: TextSpan(
+                        text: (widget.authTypeSelector ==
+                                AuthTypeSelector.register)
+                            ? "Sign In"
+                            : 'Register',
+                        style: TextStyle(color: Colors.blue),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute<void>(
+                                  builder: (_) => SignInOrRegistrationPage(
+                                      widget.authTypeSelector ==
+                                              AuthTypeSelector.register
+                                          ? AuthTypeSelector.signIn
+                                          : AuthTypeSelector.register)),
+                            );
+                          }),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          )),
-    );
+          ),
+        ));
   }
 
   @override
   void dispose() {
     super.dispose();
   }
-}
 
-Future<bool> _register(
-    BuildContext context, String username, String password) async {
-  try {
-    final User user = (await _auth.createUserWithEmailAndPassword(
-      email: username,
-      password: password,
-    ))
-        .user;
-    if (user != null) {
-      return true;
+  Future<bool> _register(String username, String password) async {
+    try {
+      final User user = (await _auth.createUserWithEmailAndPassword(
+        email: username,
+        password: password,
+      ))
+          .user;
+      if (user != null) {
+        return true;
+      }
+    } catch (e) {
+      print(e);
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text("Registration failed"),
+      ));
     }
-  } catch (e) {
-    print(e);
-    Scaffold.of(context).showSnackBar(SnackBar(
-      content: Text("Registration failed"),
-    ));
+    return false;
   }
-  return false;
-}
 
-void _signInWithEmailAndPassword(
-    BuildContext context, String username, String password) async {
-  try {
-    final User user = (await _auth.signInWithEmailAndPassword(
-      email: username,
-      password: password,
-    ))
-        .user;
-    var token = await user.getIdToken(/* forceRefresh */ true);
+  void _signInWithEmailAndPassword(String username, String password) async {
+    try {
+      final User user = (await _auth.signInWithEmailAndPassword(
+        email: username,
+        password: password,
+      ))
+          .user;
+      var token = await user.getIdToken(/* forceRefresh */ true);
 
-    print(token);
-    api.DefaultApi d = api.DefaultApi(
-        ApiClient(basePath: "https://trober-test.herokuapp.com"));
-    d.apiDelegate.apiClient.setDefaultHeader("X-TOKEN", token);
-    var auth = ApiKeyAuth("header", "X-TOKEN");
-    auth.apiKey = token;
-    d.apiDelegate.apiClient.setAuthentication('ApiKeyAuth', auth);
-    print(await d.tenantsGet());
-    var prefs = await SharedPreferences.getInstance();
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute<void>(
-          builder: (_) => DriverHomeScreen(
-                tasksInteractor: TasksInteractor(
-                  ReactiveLocalStorageRepository(
-                    repository: LocalStorageRepository(
-                      localStorage: KeyValueStorage(
-                        'trober_tasks',
-                        FlutterKeyValueStore(prefs),
+      print(token);
+      api.DefaultApi d = api.DefaultApi(
+          ApiClient(basePath: "https://trober-test.herokuapp.com"));
+      d.apiDelegate.apiClient.setDefaultHeader("X-TOKEN", token);
+      var auth = ApiKeyAuth("header", "X-TOKEN");
+      auth.apiKey = token;
+      d.apiDelegate.apiClient.setAuthentication('ApiKeyAuth', auth);
+      print(await d.tenantsGet());
+      var prefs = await SharedPreferences.getInstance();
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute<void>(
+            builder: (_) => DriverHomeScreen(
+                  tasksInteractor: TasksInteractor(
+                    ReactiveLocalStorageRepository(
+                      repository: LocalStorageRepository(
+                        localStorage: KeyValueStorage(
+                          'trober_tasks',
+                          FlutterKeyValueStore(prefs),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                userRepository: AnonymousUserRepository(),
-              )),
-    );
-  } catch (e) {
-    Scaffold.of(context).showSnackBar(SnackBar(
-      content: Text("Failed to sign in with Email & Password"),
-    ));
+                  userRepository: AnonymousUserRepository(),
+                )),
+      );
+    } catch (e) {
+      print(e);
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text("Failed to sign in with Email & Password"),
+      ));
+    }
   }
 }
 
