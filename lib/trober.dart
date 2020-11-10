3cf27d7ad7fd9862cf357f757b4359f0cbf3ace1
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-09 21:10:09 -0800

    Remove api generators

--- lib/trober.dart
+++
@@ -1,9 +0,0 @@
-import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
-
-@Openapi(
-    additionalProperties: AdditionalProperties(
-        pubName: 'trober_api', pubAuthor: 'Big Panther Inc.'),
-    inputSpecFile: 'lib/trober/trober.yaml',
-    generatorName: Generator.DART2_API,
-    outputDirectory: 'api/trober_api')
-class Trober extends OpenapiGeneratorConfig {}
