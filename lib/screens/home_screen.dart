9637d67f3eac1ec0fbcdaeffd72b1fcb1018dc23
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-11 13:24:39 -0800

    Remove user repo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,14 +7,11 @@ import 'dart:async';
 import 'package:shipanther/blocs/models/visibility_filter.dart';
 import 'package:shipanther/blocs/stats_bloc.dart';
 import 'package:shipanther/blocs/tasks_list_bloc.dart';
-import 'package:shipanther/blocs/user_bloc.dart';
 import 'package:shipanther/dependency_injection.dart';
 import 'package:shipanther/localization.dart';
 import 'package:shipanther/tasks_app_core/keys.dart';
 import 'package:shipanther/tasks_app_core/localization.dart';
 import 'package:shipanther/tasks_app_core/routes.dart';
-import 'package:shipanther/tasks_repository_core/user_entity.dart';
-import 'package:shipanther/tasks_repository_core/user_repository.dart';
 import 'package:shipanther/widgets/extra_actions_button.dart';
 import 'package:shipanther/widgets/filter_button.dart';
 import 'package:shipanther/widgets/loading.dart';
@@ -22,16 +19,12 @@ import 'package:shipanther/widgets/stats_counter.dart';
 import 'package:shipanther/widgets/task_list.dart';
 import 'package:shipanther/widgets/tasks_bloc_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 import 'package:rxdart/rxdart.dart';
 
 enum AppTab { tasks, stats }
 
 class HomeScreen extends StatefulWidget {
-  final UserRepository repository;
-
-  HomeScreen({@required this.repository})
-      : super(key: ArchSampleKeys.homeScreen);
+  HomeScreen() : super(key: ArchSampleKeys.homeScreen);
 
   @override
   State<StatefulWidget> createState() {
@@ -40,14 +33,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
-  UserBloc usersBloc;
   StreamController<AppTab> tabController;
 
   @override
   void initState() {
     super.initState();
 
-    usersBloc = UserBloc(widget.repository);
     tabController = StreamController<AppTab>();
   }
 
@@ -61,64 +52,59 @@ class HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final tasksBloc = TasksBlocProvider.of(context);
 
-    return StreamBuilder<UserEntity>(
-      stream: usersBloc.login(),
-      builder: (context, userSnapshot) {
-        return StreamBuilder<AppTab>(
-          initialData: AppTab.tasks,
-          stream: tabController.stream,
-          builder: (context, activeTabSnapshot) {
-            return Scaffold(
-              appBar: AppBar(
-                title: Text(BlocLocalizations.of(context).appTitle),
-                actions: _buildActions(
-                  tasksBloc,
-                  activeTabSnapshot,
-                  userSnapshot.hasData,
+    return StreamBuilder<AppTab>(
+      initialData: AppTab.tasks,
+      stream: tabController.stream,
+      builder: (context, activeTabSnapshot) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(BlocLocalizations.of(context).appTitle),
+            actions: _buildActions(
+              tasksBloc,
+              activeTabSnapshot,
+              true,
+            ),
+          ),
+          body: true
+              ? activeTabSnapshot.data == AppTab.tasks
+                  ? TaskList()
+                  : StatsCounter(
+                      buildBloc: () =>
+                          StatsBloc(Injector.of(context).tasksInteractor),
+                    )
+              : LoadingSpinner(
+                  key: ArchSampleKeys.tasksLoading,
                 ),
-              ),
-              body: userSnapshot.hasData
-                  ? activeTabSnapshot.data == AppTab.tasks
-                      ? TaskList()
-                      : StatsCounter(
-                          buildBloc: () =>
-                              StatsBloc(Injector.of(context).tasksInteractor),
-                        )
-                  : LoadingSpinner(
-                      key: ArchSampleKeys.tasksLoading,
-                    ),
-              floatingActionButton: FloatingActionButton(
-                key: ArchSampleKeys.addTaskFab,
-                onPressed: () {
-                  Navigator.pushNamed(context, ArchSampleRoutes.addTask);
-                },
-                child: Icon(Icons.add),
-                tooltip: ArchSampleLocalizations.of(context).addTask,
-              ),
-              bottomNavigationBar: BottomNavigationBar(
-                key: ArchSampleKeys.tabs,
-                currentIndex: AppTab.values.indexOf(activeTabSnapshot.data),
-                onTap: (index) {
-                  tabController.add(AppTab.values[index]);
-                },
-                items: AppTab.values.map((tab) {
-                  return BottomNavigationBarItem(
-                    icon: Icon(
-                      tab == AppTab.tasks ? Icons.list : Icons.show_chart,
-                      key: tab == AppTab.stats
-                          ? ArchSampleKeys.statsTab
-                          : ArchSampleKeys.taskTab,
-                    ),
-                    title: Text(
-                      tab == AppTab.stats
-                          ? ArchSampleLocalizations.of(context).stats
-                          : ArchSampleLocalizations.of(context).tasks,
-                    ),
-                  );
-                }).toList(),
-              ),
-            );
-          },
+          floatingActionButton: FloatingActionButton(
+            key: ArchSampleKeys.addTaskFab,
+            onPressed: () {
+              Navigator.pushNamed(context, ArchSampleRoutes.addTask);
+            },
+            child: Icon(Icons.add),
+            tooltip: ArchSampleLocalizations.of(context).addTask,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            key: ArchSampleKeys.tabs,
+            currentIndex: AppTab.values.indexOf(activeTabSnapshot.data),
+            onTap: (index) {
+              tabController.add(AppTab.values[index]);
+            },
+            items: AppTab.values.map((tab) {
+              return BottomNavigationBarItem(
+                icon: Icon(
+                  tab == AppTab.tasks ? Icons.list : Icons.show_chart,
+                  key: tab == AppTab.stats
+                      ? ArchSampleKeys.statsTab
+                      : ArchSampleKeys.taskTab,
+                ),
+                title: Text(
+                  tab == AppTab.stats
+                      ? ArchSampleLocalizations.of(context).stats
+                      : ArchSampleLocalizations.of(context).tasks,
+                ),
+              );
+            }).toList(),
+          ),
         );
       },
     );
