17440ad08430302e5d3202638083305c6c112ed2
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-12 17:53:43 -0800

    Add tenant repo

--- lib/data/tenant/remote_tenant_repository.dart
+++ lib/data/tenant/remote_tenant_repository.dart
@@ -1,5 +1,4 @@
 import 'package:shipanther/data/api/api_repository.dart';
-import 'package:shipanther/data/auth/auth_repository.dart';
 import 'package:shipanther/data/tenant/tenant_repository.dart';
 import 'package:trober_sdk/api.dart';
 
@@ -8,14 +7,26 @@ class RemoteTenantRepository extends TenantRepository {
 
   const RemoteTenantRepository(this._apiRepository);
   @override
-  Future<Tenant> fetchTenant(String id) {
-    // TODO: implement fetchTenant
-    throw UnimplementedError();
+  Future<Tenant> fetchTenant(String id) async {
+    var client = await _apiRepository.apiClient();
+    return await client.tenantsIdGet(id);
   }
 
   @override
-  Future<List<Tenant>> fetchTenants() {
-    // TODO: implement fetchTenants
-    throw UnimplementedError();
+  Future<List<Tenant>> fetchTenants() async {
+    var client = await _apiRepository.apiClient();
+    return await client.tenantsGet();
+  }
+
+  @override
+  Future<Tenant> createTenant(Tenant tenant) async {
+    var client = await _apiRepository.apiClient();
+    return await client.tenantsPost(tenant: tenant);
+  }
+
+  @override
+  Future<Tenant> updateTenant(String id, Tenant tenant) async {
+    var client = await _apiRepository.apiClient();
+    return await client.tenantsIdPatch(id, tenant: tenant);
   }
 }
