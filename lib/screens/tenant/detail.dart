ccd47918044e80f2748388f101c7bccae725e3af
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-13 17:26:25 -0800

    Seperate tenant widgets

---
+++ lib/screens/tenant/detail.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:shipanther/bloc/tenant/tenant_bloc.dart';
+
+class TenantDetail extends StatelessWidget {
+  final TenantLoaded state;
+  const TenantDetail({Key key, this.state}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tenant Details"),
+        actions: [
+          IconButton(
+            tooltip: "Delete Tenant",
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              // context
+              //     .read<TenantBloc>()
+              //     .add(DeleteTenant(state.tenant.id));
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 8.0,
+                          bottom: 16.0,
+                        ),
+                        child: Text(
+                          state.tenant.name,
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                      ),
+                      Text(
+                        'Id  ${state.tenant.id}',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                      Text(
+                        'Created At  ${state.tenant.createdAt}',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                      Text(
+                        'Type  ${state.tenant.type}',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                      Text(
+                        'Updated At  ${state.tenant.updatedAt}',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                      Text(
+                        'Created By  ${state.tenant.createdBy}',
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Edit tenant",
+        child: Icon(Icons.edit),
+        onPressed: () {
+          //context.read<TenantBloc>().add()
+        },
+      ),
+    );
+  }
+}
