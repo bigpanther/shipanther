cb9337fa4ac737ef87e8024c79f2432f6f577964
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-16 19:49:41 -0800

    Add logout option

--- lib/screens/tenant/list.dart
+++ lib/screens/tenant/list.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shipanther/bloc/tenant/tenant_bloc.dart';
 import 'package:shipanther/screens/tenant/add_edit.dart';
 import 'package:shipanther/widgets/filter_button.dart';
+import 'package:shipanther/widgets/shipanther_scaffold.dart';
 import 'package:trober_sdk/api.dart';
 
 class TenantList extends StatelessWidget {
@@ -13,54 +14,55 @@ class TenantList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Tenants"),
-        actions: [
-          FilterButton<TenantType>(
-            possibleValues: TenantType.values,
-            isActive: true,
-            activeFilter: tenantLoadedState.tenantType,
-            onSelected: (t) => context.read<TenantBloc>()..add(GetTenants(t)),
-            tooltip: "Filter Tenant type",
-          )
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: tenantLoadedState.tenants.length,
-        itemBuilder: (BuildContext context, int index) {
-          var t = tenantLoadedState.tenants.elementAt(index);
-          return ListTile(
-            onTap: () => tenantBloc.add(GetTenant(t.id)),
-            title: Text(
-              t.name,
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            subtitle: Text(
-              "Created: ${t.createdAt}",
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: "Add tenant",
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => TenantAddEdit(
-                isEdit: false,
-                tenantBloc: tenantBloc,
-                tenant: Tenant(),
-              ),
-            ),
-          );
-        },
-      ),
+    var title = "Tenants";
+    List<Widget> actions = [
+      FilterButton<TenantType>(
+        possibleValues: TenantType.values,
+        isActive: true,
+        activeFilter: tenantLoadedState.tenantType,
+        onSelected: (t) => context.read<TenantBloc>()..add(GetTenants(t)),
+        tooltip: "Filter Tenant type",
+      )
+    ];
+    Widget body = ListView.builder(
+      itemCount: tenantLoadedState.tenants.length,
+      itemBuilder: (BuildContext context, int index) {
+        var t = tenantLoadedState.tenants.elementAt(index);
+        return ListTile(
+          onTap: () => tenantBloc.add(GetTenant(t.id)),
+          title: Text(
+            t.name,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          subtitle: Text(
+            "Created: ${t.createdAt}",
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        );
+      },
     );
+    Widget floatingActionButton = FloatingActionButton(
+      tooltip: "Add tenant",
+      child: Icon(Icons.add),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => TenantAddEdit(
+              isEdit: false,
+              tenantBloc: tenantBloc,
+              tenant: Tenant(),
+            ),
+          ),
+        );
+      },
+    );
+    return ShipantherScaffold(
+        title: title,
+        actions: actions,
+        body: body,
+        floatingActionButton: floatingActionButton);
   }
 }
