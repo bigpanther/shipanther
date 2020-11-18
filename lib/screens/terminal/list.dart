66fcf97cdfaff60b107384afb9edf65b82a1139f
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-18 12:47:07 -0800

    Add drawer to the list page

--- lib/screens/terminal/list.dart
+++ lib/screens/terminal/list.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shipanther/bloc/terminal/terminal_bloc.dart';
+import 'package:shipanther/l10n/shipanther_localization.dart';
 import 'package:shipanther/screens/terminal/add_edit.dart';
 import 'package:shipanther/widgets/filter_button.dart';
+import 'package:shipanther/widgets/shipanther_scaffold.dart';
 import 'package:trober_sdk/api.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -15,55 +17,56 @@ class TerminalList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Terminals"),
-        actions: [
-          FilterButton<TerminalType>(
-            possibleValues: TerminalType.values,
-            isActive: true,
-            activeFilter: terminalLoadedState.terminalType,
-            onSelected: (t) =>
-                context.read<TerminalBloc>()..add(GetTerminals(t)),
-            tooltip: "Filter Terminal type",
-          )
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: terminalLoadedState.terminals.length,
-        itemBuilder: (BuildContext context, int index) {
-          var t = terminalLoadedState.terminals.elementAt(index);
-          return ListTile(
-            onTap: () => terminalBloc.add(GetTerminal(t.id)),
-            title: Text(
-              t.name,
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            subtitle: Text(
-              "Created: ${t.createdAt}",
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: "Add terminal",
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => TerminalAddEdit(
-                isEdit: false,
-                terminalBloc: terminalBloc,
-                terminal: Terminal(),
-              ),
-            ),
-          );
-        },
-      ),
+    var title = ShipantherLocalizations.of(context).terminalsTitle;
+    List<Widget> actions = [
+      FilterButton<TerminalType>(
+        possibleValues: TerminalType.values,
+        isActive: true,
+        activeFilter: terminalLoadedState.terminalType,
+        onSelected: (t) => context.read<TerminalBloc>()..add(GetTerminals(t)),
+        tooltip: "Filter Terminal type",
+      )
+    ];
+    Widget body = ListView.builder(
+      itemCount: terminalLoadedState.terminals.length,
+      itemBuilder: (BuildContext context, int index) {
+        var t = terminalLoadedState.terminals.elementAt(index);
+        return ListTile(
+          onTap: () => terminalBloc.add(GetTerminal(t.id)),
+          title: Text(
+            t.name,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          subtitle: Text(
+            "Created: ${t.createdAt}",
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        );
+      },
     );
+    Widget floatingActionButton = FloatingActionButton(
+      tooltip: "Add terminal",
+      child: Icon(Icons.add),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => TerminalAddEdit(
+              isEdit: false,
+              terminalBloc: terminalBloc,
+              terminal: Terminal(),
+            ),
+          ),
+        );
+      },
+    );
+
+    return ShipantherScaffold(
+        title: title,
+        actions: actions,
+        body: body,
+        floatingActionButton: floatingActionButton);
   }
 }
