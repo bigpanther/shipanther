a8b4e36071c42556fc04253abc6fbfdf29d9c97e
Harsimran Singh Maan <maan.harry@gmail.com>
2020-11-20 23:32:35 -0800

    Add customer bloc

---
+++ lib/bloc/customer/customer_event.dart
@@ -0,0 +1,4 @@
+part of 'customer_bloc.dart';
+
+@immutable
+abstract class CustomerEvent {}
---
+++ lib/bloc/customer/customer_state.dart
@@ -0,0 +1,6 @@
+part of 'customer_bloc.dart';
+
+@immutable
+abstract class CustomerState {}
+
+class CustomerInitial extends CustomerState {}
