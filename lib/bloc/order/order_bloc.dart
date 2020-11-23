581998293756b8e4a3cbe4e71e6c34afb1e52055
Anshul Chouhan <anshulchouhan2401@gmail.com>
2020-11-23 12:52:12 +0530

    Make order bloc abd repository

--- lib/bloc/order/order_bloc.dart
+++ lib/bloc/order/order_bloc.dart
@@ -3,16 +3,40 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import 'package:shipanther/data/order/order_repository.dart';
+import 'package:trober_sdk/api.dart';
+
 part 'order_event.dart';
 part 'order_state.dart';
 
 class OrderBloc extends Bloc<OrderEvent, OrderState> {
-  OrderBloc() : super(OrderInitial());
+  final OrderRepository _orderRepository;
+  OrderBloc(this._orderRepository) : super(OrderInitial());
 
   @override
   Stream<OrderState> mapEventToState(
     OrderEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield OrderLoading();
+    if (event is GetOrder) {
+      yield OrderLoaded(await _orderRepository.fetchOrder(event.id));
+    }
+    if (event is GetOrders) {
+      var orders = await _orderRepository.fetchOrdersOfTenant(event.tenantId);
+      yield OrdersLoaded(orders, event.tenantId);
+    }
+    if (event is UpdateOrder) {
+      await _orderRepository.updateOrder(event.id, event.order);
+      var orders = await _orderRepository.fetchOrdersOfTenant(null);
+      yield OrdersLoaded(orders, null);
+    }
+    if (event is CreateOrder) {
+      await _orderRepository.createOrder(event.order);
+      var orders = await _orderRepository.fetchOrdersOfTenant(null);
+      yield OrdersLoaded(orders, null);
+    }
+    if (event is DeleteOrder) {
+      yield OrderFailure("Tenant deletion is not supported");
+    }
   }
 }
--- lib/bloc/order/order_event.dart
+++ lib/bloc/order/order_event.dart
@@ -1,4 +1,32 @@
 part of 'order_bloc.dart';
 
 @immutable
-abstract class OrderEvent {}
+abstract class OrderEvent {
+  const OrderEvent();
+}
+
+class GetOrder extends OrderEvent {
+  final String id;
+  const GetOrder(this.id);
+}
+
+class UpdateOrder extends OrderEvent {
+  final String id;
+  final Order order;
+  const UpdateOrder(this.id, this.order);
+}
+
+class CreateOrder extends OrderEvent {
+  final Order order;
+  const CreateOrder(this.order);
+}
+
+class DeleteOrder extends OrderEvent {
+  final String id;
+  const DeleteOrder(this.id);
+}
+
+class GetOrders extends OrderEvent {
+  final String tenantId;
+  const GetOrders(this.tenantId);
+}
