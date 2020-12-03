2901d9220542f944ec1830029e0d984992ad6d37
Harsimran Singh Maan <maan.harry@gmail.com>
2020-12-02 20:29:20 -0800

    Fix flutter analyze errors

--- lib/screens/none_home.dart
+++ lib/screens/none_home.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shipanther/l10n/shipanther_localization.dart';
 import 'package:shipanther/widgets/shipanther_scaffold.dart';
