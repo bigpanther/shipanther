fc01541d1a121bcc2ff8cb6805d711767fc79757
Harsimran Singh Maan <maan.harry@gmail.com>
2020-12-08 15:12:57 -0800

    Update customer screen

--- lib/screens/customer_home.dart
+++ lib/screens/customer_home.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shipanther/bloc/terminal/terminal_bloc.dart';
+import 'package:shipanther/bloc/order/order_bloc.dart';
 import 'package:shipanther/l10n/shipanther_localization.dart';
-import 'package:shipanther/screens/terminal/list.dart';
+import 'package:shipanther/screens/order/list.dart';
 import 'package:shipanther/widgets/centered_loading.dart';
 import 'package:shipanther/widgets/shipanther_scaffold.dart';
 import 'package:trober_sdk/api.dart';
@@ -16,28 +16,28 @@ class CustomerHome extends StatefulWidget {
 }
 
 class _CustomerHomeState extends State<CustomerHome> {
-  TerminalBloc bloc;
+  OrderBloc bloc;
   @override
   void initState() {
     super.initState();
-    bloc = context.read<TerminalBloc>();
-    bloc.add(GetTerminals(null));
+    bloc = context.read<OrderBloc>();
+    bloc.add(GetOrders(null));
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<TerminalBloc, TerminalState>(
+    return BlocConsumer<OrderBloc, OrderState>(
       listener: (context, state) {
-        if (state is TerminalFailure) {
+        if (state is OrderFailure) {
           Scaffold.of(context).showSnackBar(SnackBar(
             content: Text(state.message),
           ));
         }
       },
       builder: (context, state) {
-        if (state is TerminalsLoaded) {
-          return TerminalList(widget.user,
-              terminalBloc: bloc, terminalLoadedState: state);
+        if (state is OrdersLoaded) {
+          return OrderList(widget.user,
+              orderBloc: bloc, orderLoadedState: state);
         }
         return ShipantherScaffold(
           widget.user,
