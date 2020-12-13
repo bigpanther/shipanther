8acec42dbc90dd1e089ebf19b3508d9f12ec4edd
Harsimran Singh Maan <maan.harry@gmail.com>
2020-12-12 20:11:18 -0800

    Update localization

--- lib/l10n/locales/messages_pa.dart
+++ lib/l10n/locales/messages_pa.dart
@@ -19,8 +19,45 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'pa';
 
+  static m0(emailId) => "${emailId} ਨੂੰ ਈਮੇਲ ਭੇਜੀ ਗਈ ਹੈ. ਕਿਰਪਾ ਕਰਕੇ ਆਪਣੇ ਇਨਬਾਕਸ ਦੀ ਜਾਂਚ ਕਰੋ।";
+
+  static m1(param) => "${param} ਦੀ ਲੋੜ ਹੈ";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
   static _notInlinedMessages(_) => <String, Function> {
-    "tenantsTitle" : MessageLookupByLibrary.simpleMessage("ਕਿਰਾਏਦਾਰ")
+    "carriersTitle" : MessageLookupByLibrary.simpleMessage("ਕੈਰੀਅਰ"),
+    "changePassword" : MessageLookupByLibrary.simpleMessage("ਪਾਸਵਰਡ ਬਦਲੋ"),
+    "confirmPassword" : MessageLookupByLibrary.simpleMessage("ਪਾਸਵਰਡ ਪੱਕਾ ਕਰੋ"),
+    "containersTitle" : MessageLookupByLibrary.simpleMessage("ਕੰਟੇਨਰ"),
+    "create" : MessageLookupByLibrary.simpleMessage("ਬਣਾਓ"),
+    "customersTitle" : MessageLookupByLibrary.simpleMessage("ਗਾਹਕ"),
+    "edit" : MessageLookupByLibrary.simpleMessage("ਸੰਪਾਦਿਤ ਕਰੋ"),
+    "email" : MessageLookupByLibrary.simpleMessage("ਈ-ਮੇਲ"),
+    "emailNotVerified" : MessageLookupByLibrary.simpleMessage("ਇਹ ਈਮੇਲ ਆਈਡੀ ਅਜੇ ਪ੍ਰਮਾਣਿਤ ਨਹੀਂ ਹੈ. ਮੁੜ ਕੋਸ਼ਿਸ ਕਰੋ ਜੀ."),
+    "emailSent" : m0,
+    "forgotPassword" : MessageLookupByLibrary.simpleMessage("ਪਾਸਵਰਡ ਭੁੱਲ ਗਏ?"),
+    "home" : MessageLookupByLibrary.simpleMessage("ਮੁੱਖ ਪੰਨਾ"),
+    "logout" : MessageLookupByLibrary.simpleMessage("ਲਾੱਗ ਆਊਟ"),
+    "name" : MessageLookupByLibrary.simpleMessage("ਨਾਮ"),
+    "newPassword" : MessageLookupByLibrary.simpleMessage("ਨਵਾਂ ਪਾਸਵਰਡ"),
+    "noDateChosen" : MessageLookupByLibrary.simpleMessage("ਕੋਈ ਤਾਰੀਖ ਨਹੀਂ ਚੁਣੀ ਗਈ"),
+    "oldPassword" : MessageLookupByLibrary.simpleMessage("ਪੁਰਾਣਾ ਪਾਸਵਰਡ"),
+    "ordersTitle" : MessageLookupByLibrary.simpleMessage("ਆਰਡਰਸ"),
+    "paramRequired" : m1,
+    "passowrdDoesntMatch" : MessageLookupByLibrary.simpleMessage("ਪਾਸਵਰਡ ਮੇਲ ਨਹੀਂ ਖਾਂਦਾ"),
+    "password" : MessageLookupByLibrary.simpleMessage("ਪਾਸਵਰਡ"),
+    "profile" : MessageLookupByLibrary.simpleMessage("ਪ੍ਰੋਫਾਈਲ"),
+    "register" : MessageLookupByLibrary.simpleMessage("ਪੰਜੀਕਰਨ"),
+    "resetPassword" : MessageLookupByLibrary.simpleMessage("ਪਾਸਵਰਡ ਰੀਸੈਟ ਕਰੋ"),
+    "resetPasswordMessage" : MessageLookupByLibrary.simpleMessage("ਰੀਸੈਟ ਲਿੰਕ ਵਾਲੀ ਇੱਕ ਈਮੇਲ ਤੁਹਾਨੂੰ ਭੇਜੀ ਜਾਏਗੀ."),
+    "save" : MessageLookupByLibrary.simpleMessage("ਰੱਖਣਾ"),
+    "settings" : MessageLookupByLibrary.simpleMessage("ਸੈਟਿੰਗਜ਼"),
+    "signIn" : MessageLookupByLibrary.simpleMessage("ਲਾੱਗ ਇਨ"),
+    "tenantLessUserMessage" : MessageLookupByLibrary.simpleMessage("ਰਜਿਸਟ੍ਰੇਸ਼ਨ ਕਰਨ ਵਾਸਤੇ ਤੁਹਾਡਾ ਬਹੁਤ ਧੰਨਵਾਦ। ਏਹ ਸੌਫਟਵੇਅਰ ਹਾਲੀ ਤਜਰਬੇ ਵਾਲੀ ਸਥਿਤੀ ਵਿੱਚ ਹੈ । ਅਸੀਂ ਤੁਹਾਨੰ ਛੇਤੀ ਹੀ ਵਾਪਿਸ ਜੁਵਾਬ ਦੇਵਾਂਗੇ। ਧੰਨਵਾਦ।"),
+    "tenantsTitle" : MessageLookupByLibrary.simpleMessage("ਕਿਰਾਏਦਾਰ"),
+    "terminalsTitle" : MessageLookupByLibrary.simpleMessage("ਟਰਮੀਨਲ"),
+    "username" : MessageLookupByLibrary.simpleMessage("ਉਪਯੋਗਕਰਤਾ ਨਾਮ"),
+    "usersTitle" : MessageLookupByLibrary.simpleMessage("ਉਪਭੋਗਤਾ"),
+    "welcome" : MessageLookupByLibrary.simpleMessage("ਸ਼ਿੱਪਐਨਥਰ ਤੁਹਾਡਾ ਸਵਾਗਤ ਕਰਦਾ ਹੈ")
   };
 }
