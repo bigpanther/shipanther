6bf15887fdf9a9eecc59f2eee37ad6996cb9c7df
Harsimran Singh Maan <maan.harry@gmail.com>
2021-01-15 19:50:37 -0800

    Fix build failure'

--- lib/screens/carrier/add_edit.dart
+++ lib/screens/carrier/add_edit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shipanther/bloc/carrier/carrier_bloc.dart';
 import 'package:shipanther/l10n/shipanther_localization.dart';
 import 'package:shipanther/widgets/date_time_picker.dart';
