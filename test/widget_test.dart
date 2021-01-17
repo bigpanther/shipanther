127ef7baeae6cbed03cbb467e28f844f424e95c6
Harsimran Singh Maan <maan.harry@gmail.com>
2021-01-16 23:06:34 -0800

    Add first test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,17 +6,49 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shipanther/main_common.dart';
+import 'package:shipanther/bloc/auth/auth_bloc.dart';
+import 'package:shipanther/data/auth/auth_repository.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shipanther/l10n/shipanther_localization.dart';
+import 'package:shipanther/screens/signin_or_register_page.dart';
+import 'package:shipanther/widgets/centered_loading.dart';
+import 'package:shipanther/widgets/shipanther_text_form_field.dart';
+
+class MockAuthRepository extends Mock implements AuthRepository {}
 
 void main() {
-  testWidgets('Expect sign in on start', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const ShipantherApp('https://dummy.shipanther.ca'));
+  group('SignIn Page', () {
+    late AuthRepository authRepository;
 
-    expect(find.byIcon(Icons.verified_user), findsOneWidget);
-    expect(find.byIcon(Icons.person_add), findsNothing);
-    final button = find.byType(TextButton);
-    expect(button, findsOneWidget);
+    setUp(() {
+      authRepository = MockAuthRepository();
+    });
+
+    testWidgets('renders the login page when not logged in', (tester) async {
+      when(authRepository.logIn()).thenThrow(
+        UnAuthenticatedException(),
+      );
+      await tester.pumpWidget(BlocProvider.value(
+        value: AuthBloc(authRepository),
+        child: MaterialApp(
+          localizationsDelegates: const [
+            ShipantherLocalizationsDelegate(),
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales:
+              ShipantherLocalizations.supportedLocales.map((e) => Locale(e)),
+          home: SignInOrRegistrationPage(),
+        ),
+      ));
+      await tester.pumpAndSettle();
+      expect(find.byType(SignInOrRegistrationPage), findsOneWidget);
+      expect(find.byType(ShipantherButton), findsOneWidget);
+      expect(find.byType(ShipantherTextFormField), findsNWidgets(2));
+    });
   });
 }
